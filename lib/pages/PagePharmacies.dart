2b9fc1bef151ef3c273df08e896882ea9fb8adc0
ToxicLuly <laurabutler2012@gmail.com>
2023-04-14 13:53:56 +0200

    bug of images of news has been resolved.

--- lib/pages/PagePharmacies.dart
+++ lib/pages/PagePharmacies.dart
@@ -279,12 +279,13 @@ String renderTextTraslated(String name, BuildContext context) {
 
 Widget renderImagePharmacy(Pharmacy pharmacy) {
   if (pharmacy.imageUrl == null) {
-    return const SizedBox(
-        width: 20.0, height: 20.0, child: CircularProgressIndicator());
+    return SizedBox(
+        width: 20.0, height: 20.0, child: Image.asset('pharmacies.png'));
   } else {
     return Image.network(pharmacy.imageUrl!,
         fit: BoxFit.fill, height: 200, width: 300);
   }
+
 }
 Widget renderImageToTab(String tabName){
   switch(tabName){
